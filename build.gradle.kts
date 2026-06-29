263cd3be38b4464ca1d61fb1a297245e5a8d3a45
ProperSAMA <997794945@qq.com>
2026-06-29 14:14:24 +0800

    chore: 将retrofuturagradle版本更新至1.4.1
    - 解决远端不存在1.4.0，无法顺利构建的问题

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("maven-publish")
     id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
     id("eclipse")
-    id("com.gtnewhorizons.retrofuturagradle") version "1.4.0"
+    id("com.gtnewhorizons.retrofuturagradle") version "1.4.1"
 }
 
 // Project properties
